00e7029779223d103d4cf8161d544613bbb87918
Superaids <ryetheis@gmail.com>
2017-02-14 19:46:24 +0100

    Update

--- TriangleIntro/Program.cs
+++ TriangleIntro/Program.cs
@@ -10,17 +10,18 @@ namespace TriangleIntro
     {
         static void Main(string[] args)
         {
-            while (true)
+            bool cont = true;
+            while (cont)
             {
-                var list = new List<int>();
+                var list = new List<uint>();
 
                 for (int i = 1; i <= 3;)
                 {
                     Console.WriteLine($"Please input the {i} triangle side length in numbers only.");
 
-                    int res;
+                    uint res;
 
-                    if (Int32.TryParse(Console.ReadLine(), out res))
+                    if (uint.TryParse(Console.ReadLine(), out res))
                     {
                         list.Add(res);
                         Console.WriteLine("Successfully added.");
@@ -28,26 +29,57 @@ namespace TriangleIntro
                     }
                     else
                     {
-                        Console.WriteLine("Please only input numbers, without spaces or any other symbols");
+                        Console.WriteLine("Please only input numbers above 0, without spaces or any other symbols");
                     }
                 }
 
-                if (list[0] == list[1])
+                list = (from a in list
+                               orderby a descending
+                               select a).ToList();
+
+                if ((list[1] ^ 2 * list[2] ^ 2) == (list[0] ^ 2) || list[0] == list[1] && list[1] == list[2])
                 {
-                    if (list[0] == list[2])
+                    if (list[0] == list[1])
                     {
-                        Console.WriteLine("Equilateral triangle detected");
+                        if (list[0] == list[2])
+                        {
+                            Console.WriteLine("Equilateral triangle detected");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Isosceles triangle detected");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Isosceles triangle detected");
+                        Console.WriteLine("Scalene triangle detected");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Scalene triangle detected");
+                    Console.WriteLine($"Input of {list[0]}, {list[1]} and {list[2]} isn't a true triangle");
                 }
-                Console.ReadKey();
+
+                Console.WriteLine("Want to input another triangle? Y/N");
+
+                bool finishing = true;
+                while (finishing)
+                {
+                    switch (Console.ReadLine()?.ToLower())
+                    {
+                        case "y":
+                            cont = true;
+                            finishing = false;
+                            break;
+                        case "n":
+                            cont = false;
+                            finishing = false;
+                            break;
+                        default:
+                            Console.WriteLine("Sorry, I didn't understand the input. Please write Y if you want to input another triangle. N if you want to stop");
+                            break;
+                    }
+                }
             }
         }
     }
